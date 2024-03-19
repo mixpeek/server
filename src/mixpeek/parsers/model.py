eece88f6134b044a8cc84a0436685751ce83dc2e
goudete <goudetenrique@gmail.com>
2024-03-19 15:48:37 -0400

    exceptions:

--- src/mixpeek/parsers/model.py
+++ src/mixpeek/parsers/model.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, validator, ValidationError
 from typing import List, Union, Optional
 from enum import Enum
+from _exceptions import BadRequestError
 
 
 class ParseFileRequest(BaseModel):
@@ -12,7 +13,13 @@ class ParseFileRequest(BaseModel):
         file_url = values.get("file_url") if "file_url" in values else None
         contents = v
         if file_url is None and contents is None:
-            raise ValueError("Either 'file_url' or 'contents' must be provided.")
+            raise BadRequestError(
+                error={"message": "Either 'file_url' or 'contents' must be provided."}
+            )
         if file_url is not None and contents is not None:
-            raise ValueError("Only one of 'file_url' or 'contents' can be provided.")
+            raise BadRequestError(
+                error={
+                    "message": "Only one of 'file_url' or 'contents' can be provided."
+                }
+            )
         return v
